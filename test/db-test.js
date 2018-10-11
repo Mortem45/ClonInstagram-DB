912855fd1da2468447d075a49759dc102a4190cd
Mortem45 <ste45_007@hotmail.com>
2018-10-11 17:41:30 -0600

    guardar usuarios y encriptar el password

--- test/db-test.js
+++ test/db-test.js
@@ -5,26 +5,28 @@ const Db = require('../')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
 const fixtures = require('./fixtures')
+const utils = require('../lib/utils')
 
-const dbName = `instagram_${uuid.v4()}`
-const db = new Db({ db: dbName })
-
-test.before('setup database', async t => {
+test.beforeEach('setup database', async t => {
+  const dbName = `instagram_${uuid.v4()}`
+  const db = new Db({ db: dbName })
   await db.connect()
+  t.context.db = db
+  t.context.dbName = dbName
   t.true(db.connected, 'should be connected')
 })
 
-test.after('disconnect database', async t => {
+test.afterEach.always('cleanup database', async t => {
+  let db = t.context.db
+  let dbName = t.context.dbName
   await db.disconnect()
   t.false(db.connected, 'should be disconnected')
-})
-
-test.after.always('cleanup database', async t => {
   let conn = await r.connect({})
   await r.dbDrop(dbName).run(conn)
 })
 
 test('save image', async t => {
+  let db = t.context.db
   t.is(typeof db.saveImage, 'function', 'saveImage is function')
 
   let image = fixtures.getImage()
@@ -42,6 +44,7 @@ test('save image', async t => {
 })
 
 test('like image', async t => {
+  let db = t.context.db
   t.is(typeof db.likeImage, 'function', 'likeImage is a function')
 
   let image = fixtures.getImage()
@@ -53,6 +56,7 @@ test('like image', async t => {
 })
 
 test('get image', async t => {
+  let db = t.context.db
   t.is(typeof db.getImage, 'function', 'getImage is a function')
 
   let image = fixtures.getImage()
@@ -60,4 +64,35 @@ test('get image', async t => {
   let result = await db.getImage(created.public_id)
 
   t.deepEqual(created, result)
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+  let images = fixtures.getImages(3)
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImages()
+  t.is(created.length, result.length)
+})
+
+test('encrypt password', t => {
+  let password = 'foo123'
+  let encrypted = '02b353bf5358995bc7d193ed1ce9c2eaec2b694b21d2f96232c9d6a0832121d1'
+  let result = utils.encrypt(password)
+  t.is(result, encrypted)
+})
+
+test('save user', async t => {
+  let db = t.context.db
+  t.is(typeof db.saveUser, 'function', 'saveUser is a function')
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  let created = await db.saveUser(user)
+
+  t.is(user.username, created.username)
+  t.is(user.email, created.email)
+  t.is(user.name, created.name)
+  t.is(utils.encrypt(plainPassword), created.password)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
 })
--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -11,6 +11,21 @@ const fixtures = {
       liked: false,
       user_id: uuid.uuid()
     }
+  },
+  getImages (n) {
+    let images = []
+    while (n-- > 0) {
+      images.push(this.getImage())
+    }
+    return images
+  },
+  getUser () {
+    return {
+      name: 'A random user',
+      username: `use_id${uuid.v4()}`,
+      password: uuid.uuid(),
+      email: `${uuid.v4()}@instagram.test`
+    }
   }
 }
 
